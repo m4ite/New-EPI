189b3fb103536d97315330ca4667a0a356900d65
m4ite <maitesenaibosch@gmail.com>
2024-03-11 17:00:05 -0300

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Search from './pages/searchEPI/searchEPI';
 import Map from './pages/map/map';
 import Removes from './pages/removes/removes';
 import { Routes, Route } from 'react-router-dom';
+import Login from './pages/login/login';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
       <Route path='/Search' element={<Search/>}/>
       <Route path='/Map' element={<Map/>}/>
       <Route path='/Removes' element={<Removes/>}/>
+      <Route path='/Login' element={<Login/>}/>
+
     </Routes>
     </>
   );
---
+++ frontend/src/pages/login/login.js
@@ -0,0 +1,21 @@
+import "./style.css"
+function Login(){
+    return(
+        <div className="body">
+            <div className="container">
+                <div className="supergraphic"/>
+                <div className="logo"/>
+
+                <p className="Label">Username</p>
+                <input></input>
+
+                <p className="Label">Password</p>
+                <input></input>
+
+                <div><button>Login</button></div>
+            </div>
+        </div>
+    )
+
+}
+export default Login
