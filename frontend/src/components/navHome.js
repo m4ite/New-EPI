5d36d269f21e752d3e1b2e2316b305e8bb0e5e0d
joaoramirezz <joao.rkonrath@gmail.com>
2024-03-25 14:49:04 -0300

    commit pro pull

--- frontend/src/components/navHome.js
+++ frontend/src/components/navHome.js
@@ -2,13 +2,14 @@ import "./style.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 function NavHome() {
+    const user = JSON.parse(sessionStorage.getItem("Usuario"))
     return (
         <>
             <div className="supergraphic"></div>
             <div className="line">
                 <a href="/"><FontAwesomeIcon icon={faArrowRightFromBracket} id="icon"/></a>
                 <div className="logo"></div>
-                <p>Olá, Fulano!</p>
+                <p>Olá, {user[0].User_Name}!</p>
             </div>
         </>
     )
--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -1,5 +1,44 @@
+import { useState } from "react";
 import "./style.css"
+import axios from "axios"
+
+import { useHistory } from 'react-router-dom';
+
+import { useNavigate } from 'react-router'
+import Nav from "../../components/nav";
+
 function Login(){
+
+    const [edv, setEDV] = useState()
+    const [password, setPassword] = useState()
+
+    const navigate = useNavigate()
+
+    async function TryLogin() {
+        const userLogin = {
+            "edv": edv,
+            "password": password
+        }
+
+        console.log(userLogin)
+        try {
+          const response = await axios.post('http://localhost:8080/login', userLogin);
+          
+          if (response.status == 200){
+            sessionStorage.setItem("Usuario", JSON.stringify(response.data))
+            navigate("/Home")
+          }
+
+          if (response.status == 404){
+            console.log(response.data)
+          }
+
+
+        } catch (error) {
+          console.error('Erro ao fazer requisição POST:', error.message);
+        }
+    }
+
     return(
         <div className="body">
             <div className="container">
@@ -7,14 +46,14 @@ function Login(){
                 <div className="logo"/>
 
                 <div className="forms">
-                <p className="Label">Username</p>
-                <input></input>
+                <p className="Label">EDV</p>
+                <input onChange={e => setEDV(e.target.value)}></input>
 
                 <p className="Label">Password</p>
-                <input></input>
-                
+                <input onChange={e => setPassword(e.target.value)}></input>
+
                 </div>
-                <a href="/Home"><button className="butt">Login</button></a>             
+                <a><button className="butt" onClick={() => TryLogin()}>Login</button></a>             
             </div>
         </div>
     )
